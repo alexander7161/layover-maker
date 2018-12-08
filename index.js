676fc3b62a6a729e564da0717d65f4cba4d3708d
Alexander Davis <alexanderdavis.net@googlemail.com>
2018-12-08 17:41:42 +0000

    package

--- index.js
+++ index.js
@@ -1,31 +1,32 @@
 fs = require("fs");
 var parser = require("xml2json");
+const fetch = require("node-fetch");
 
 fs.readFile("./req.xml", function(err, data) {
   var json = parser.toJson(data);
-  console.log("to json ->", json);
+  //   console.log("to json ->", json);
+
+  postData(
+    `https://18.130.255.52:9002/ndc171webservices/v171/airshopping?site=airline`,
+    parser.toXml(json)
+  )
+    .then(data => console.log(JSON.stringify(data))) // JSON-string from `response.json()` call
+    .catch(error => console.error(error));
 });
 
-// postData(
-//   `https://18.130.255.52:9002/ndc171webservices/v171/airshopping?site=airline`,
-//   { answer: 42 }
-// )
-//   .then(data => console.log(JSON.stringify(data))) // JSON-string from `response.json()` call
-//   .catch(error => console.error(error));
-
-// function postData(url = ``, data = {}) {
-//   // Default options are marked with *
-//   return fetch(url, {
-//     method: "POST", // *GET, POST, PUT, DELETE, etc.
-//     mode: "cors", // no-cors, cors, *same-origin
-//     cache: "no-cache", // *default, no-cache, reload, force-cache, only-if-cached
-//     credentials: "same-origin", // include, *same-origin, omit
-//     headers: {
-//       "Content-Type": "application/json; charset=utf-8"
-//       // "Content-Type": "application/x-www-form-urlencoded",
-//     },
-//     redirect: "follow", // manual, *follow, error
-//     referrer: "no-referrer", // no-referrer, *client
-//     body: JSON.stringify(data) // body data type must match "Content-Type" header
-//   }).then(response => response.json()); // parses response to JSON
-// }
+function postData(url = ``, data = {}) {
+  // Default options are marked with *
+  return fetch(url, {
+    method: "POST", // *GET, POST, PUT, DELETE, etc.
+    mode: "no-cors", // no-cors, cors, *same-origin
+    cache: "no-cache", // *default, no-cache, reload, force-cache, only-if-cached
+    credentials: "same-origin", // include, *same-origin, omit
+    headers: {
+      "Content-Type": "application/json; charset=utf-8"
+      // "Content-Type": "application/x-www-form-urlencoded",
+    },
+    redirect: "follow", // manual, *follow, error
+    referrer: "no-referrer", // no-referrer, *client
+    body: JSON.stringify(data) // body data type must match "Content-Type" header
+  }).then(response => response.json()); // parses response to JSON
+}
